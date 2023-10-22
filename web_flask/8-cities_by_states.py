41f70fdfed6e0169df1739d90d3aea5793c6b13b
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-10-22 08:13:05 -0700

    hbnb

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application that listens on 0.0.0.0, port 5000.
-    """
+"""
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -8,14 +8,13 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Func that Displays
-    HTML page with a list of all State objects in DBStorage.
-
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Func that displays HTML page
+    with list of all states and related cities.
     """
     states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
